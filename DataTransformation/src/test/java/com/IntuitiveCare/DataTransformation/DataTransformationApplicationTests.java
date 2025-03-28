f764edef4b726279bf8a53d984a7e4288804d2a6
MarcoShvy <mass7813@gmail.com>
2025-03-28 16:42:09 -0300

    Data Transformation project created

---
+++ DataTransformation/src/test/java/com/IntuitiveCare/DataTransformation/DataTransformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IntuitiveCare.DataTransformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataTransformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
