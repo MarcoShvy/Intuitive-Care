1e089b38e082c64cba4c761d64e135933634f075
MarcoShvy <mass7813@gmail.com>
2025-03-29 15:08:17 -0300

    update

--- DataTransformation/src/PdfReader.py
+++ DataTransformation/src/PdfReader.py
@@ -1,24 +1,26 @@
 import tabula
 import pandas as pd
 
-def extractAndConvertPDF(file_path, file_output):
+class PdfReader:
+    def __init__(self, file_path, output_file):
+        self.file_path = file_path
+        self.output_file = output_file
 
-    # LENDO ARQUIVO PDF COM O ENCODING CERTO
-    extracted_tables = tabula.read_pdf(file_path, pages="3-181", multiple_tables=True, encoding="ISO-8859-1")
+    def read_pdf(self):
+        return tabula.read_pdf(self.file_path, pages="3-181", multiple_tables=True, encoding="ISO-8859-1")
 
-    # CONCTENANDO TABELAS PARA MODIFICAR TODAS
-    extracted_tables_final = pd.concat(extracted_tables, ignore_index=True)
+    def extract_tables(self):
+        extracted_tables = self.read_pdf()
+        return pd.concat(extracted_tables, ignore_index=True)
 
-    substitutions = {
-        "OD": "Seg. Odontológica",
-        "AMB": "Seg. Ambulatorial"
-    }
+    def replace_abbreviations(self, df):
+        substitutions = {
+            "OD": "Seg. Odontológica",
+            "AMB": "Seg. Ambulatorial"
+        }
+        df.replace(substitutions, inplace=True)
+        return df
 
-    # REESCREVENDO AS SUBSTITUIÇÕES
-    extracted_tables_final.replace(substitutions, inplace=True)
-
-    # SALVANDO ARQUIVO CSV
-    extracted_tables_final.to_csv(file_output, encoding="utf-8", index=False)
-
-
-    print(f"CSV salvo em: {file_output}")
+    def save_to_csv(self, df):
+        df.to_csv(self.output_file, encoding="utf-8", index=False)
+        print(f"CSV salvo em: {self.output_file}")
