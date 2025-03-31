5e980660fde08b648a6d9f4b4260f994267eefc5
Marco <125201711+MarcoShvy@users.noreply.github.com>
2025-03-31 11:40:17 -0300

    Delete APITest/front-end directory

--- APITest/front-end/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-
-const app = createApp(App);
-
-// Certifique-se de que qualquer configuração que você esteja tentando acessar
-// está sendo inicializada corretamente
-app.mount('#app');
